1f3497aa0c623f5d12ac8ed59d635c6963ac1197
Antonia <as54033@fer.hr>
2024-01-17 01:42:27 +0100

    feat: add all

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,38 +1,59 @@
-import asyncio
 import os
+from azure.eventhub import EventHubConsumerClient, TransportType
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 from dotenv import load_dotenv, find_dotenv
-from azure.eventhub.aio import EventHubConsumerClient
 
 dotenv_path = find_dotenv()
 load_dotenv(dotenv_path)
 
+# Azure Event Hub Configuration
 EVENT_HUB_CONNECTION_STR = os.getenv("EVENT_HUB_CONNECTION_STR")
 EVENT_HUB_NAME = os.getenv("EVENT_HUB_NAME")
 
+# Azure Data Lake Storage Configuration
+ADLS_CONNECTION_STR = os.getenv("ADLS_CONNECTION_STR")
+ADLS_CONTAINER_NAME = os.getenv("ADLS_CONTAINER_NAME")
 
-async def on_event(partition_context, event):
-    # Print the event data.
-    print(
-        'Received the event: "{}" from the partition with ID: "{}"'.format(
-            event.body_as_str(encoding="UTF-8"), partition_context.partition_id
-        )
+
+def save_to_datalake(data, timestamp):
+
+    blob_service_client = BlobServiceClient.from_connection_string(ADLS_CONNECTION_STR)
+    container_client = blob_service_client.get_container_client(ADLS_CONTAINER_NAME)
+
+    year, month, day, hour, minute = timestamp.strftime('%Y %m %d %H %M').split()
+
+    directory_path = f"{year}/{month}/{day}/{hour}/{minute}/"
+    blob_path = directory_path + f"{timestamp}.json"
+
+    blob_client = container_client.get_blob_client(blob_path)
+    blob_client.upload_blob(data)
+
+
+def on_event(partition_context, event):
+    timestamp = event.enqueued_time
+    data = event.body_as_str()
+    
+    print(timestamp)
+    print(data)
+    save_to_datalake(data, timestamp)
+    print(f"Data saved to Data Lake at {timestamp}")
+
+
+def main():
+    consumer_group = '$Default'
+
+    consumer_client = EventHubConsumerClient.from_connection_string(
+        EVENT_HUB_CONNECTION_STR, consumer_group, eventhub_name=EVENT_HUB_NAME, transport_type=TransportType.Amqp
     )
 
-
-async def main():
-
-    # Create a consumer client for the event hub.
-    client = EventHubConsumerClient.from_connection_string(
-        EVENT_HUB_CONNECTION_STR,
-        consumer_group="$Default",
-        eventhub_name=EVENT_HUB_NAME
-    )
-    async with client:
-        # Call the receive method. Read from the beginning of the
-        # partition (starting_position: "-1")
-        await client.receive(on_event=on_event, starting_position="-1")
+    try:
+        with consumer_client:
+            consumer_client.receive(
+                on_event=on_event,
+                starting_position="-1",  # Start from the latest available event
+            )
+    except KeyboardInterrupt:
+        print("Receiving has stopped.")
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    # Run the main method.
-    loop.run_until_complete(main())
+    main()
--- producer/producer.py
+++ producer/producer.py
@@ -2,6 +2,7 @@ import asyncio
 import requests
 import time
 import os
+import json
 from dotenv import load_dotenv, find_dotenv
 from azure.eventhub import EventData
 from azure.eventhub.aio import EventHubProducerClient
@@ -25,27 +26,46 @@ async def run(data):
         event_data_batch = await producer.create_batch()
 
         # Add events to the batch.
-        event_data_batch.add(EventData("Post: \n"))
-        event_data_batch.add(EventData(data))
+        event_data_batch.add(EventData(json.dumps(data)))
 
         # Send the batch of events to the event hub.
         await producer.send_batch(event_data_batch)
 
 
 
-# Dohvaćanje podataka s API-ja
-response = requests.get(api_url, headers={'User-agent': 'tpiuo'})
-if response.status_code == 200:
-    # Uzimanje podataka o objavama
+# Reddit API endpoint
+reddit_endpoint = "https://www.reddit.com/top.json"
+
+# Reddit API parameters
+params = {
+    "limit": 10,
+    "after": None,
+}
+
+# Number of posts to fetch in each batch
+batch_size = 10
+
+# Initial setup for pagination
+posts_count = 0
+
+while True:
+    # Fetch the next batch of top posts
+    response = requests.get(reddit_endpoint, params=params, headers={"User-agent": "your_user_agent"})
+    data = response.json()
     posts = response.json()["data"]["children"]
 
-    # Slanje podataka na Event Hub
     for post in posts:
-        asyncio.run(run(str(post)))
+        asyncio.run(run(post))
 
-    # Beskonačna petlja
-    while True:
-        time.sleep(1)  # Pauza od 1 sekunde kako bi se spriječilo zatvaranje programa
-else:
-    print("Error fetching data from Reddit API. Status code:", response.status_code)
+    # Update the 'after' parameter for the next batch
+    params["after"] = data['data']['after']
+    posts_count += batch_size
 
+    # Check if there are more posts
+    if posts_count >= 1000:  # Limit to 1000 posts
+        break
+
+    time.sleep(10)
+
+while True:
+    time.sleep(1)
